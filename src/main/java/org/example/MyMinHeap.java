8ade02e28cdcda32f859c82a82b3e8e7993c7abc
nurbo323 <242214@astanait.edu.kz>
2025-04-11 18:03:25 +0500

    Added MyMinHeap implementation using MyArrayList

---
+++ src/main/java/org/example/MyMinHeap.java
@@ -0,0 +1,77 @@
+package org.example;
+
+public class MyMinHeap<T extends Comparable<T>> {
+    private MyArrayList<T> heap;
+
+    public MyMinHeap() {
+        heap = new MyArrayList<>();
+    }
+
+    public void insert(T item) {
+        heap.add(item);
+        upHeap(heap.size() - 1);
+    }
+
+    public T peekMin() {
+        if (isEmpty()) return null;
+        return heap.get(0);
+    }
+
+    public T removeMin() {
+        if (isEmpty()) return null;
+        T min = heap.get(0);
+        T last = heap.get(heap.size() - 1);
+        heap.set(0, last);
+        heap.remove(heap.size() - 1);
+        downHeap(0);
+        return min;
+    }
+
+    public int size() {
+        return heap.size();
+    }
+
+    public boolean isEmpty() {
+        return heap.size() == 0;
+    }
+
+    private void upHeap(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (heap.get(index).compareTo(heap.get(parent)) < 0) {
+                swap(index, parent);
+                index = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void downHeap(int index) {
+        int size = heap.size();
+        while (index < size) {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int smallest = index;
+
+            if (left < size && heap.get(left).compareTo(heap.get(smallest)) < 0) {
+                smallest = left;
+            }
+            if (right < size && heap.get(right).compareTo(heap.get(smallest)) < 0) {
+                smallest = right;
+            }
+            if (smallest != index) {
+                swap(index, smallest);
+                index = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void swap(int i, int j) {
+        T temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
+}
